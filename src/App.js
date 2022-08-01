8780cefffa07eb6e053dd9c2a5b0612f9212725f
gkumar-pandey <gautamshekhar078@gmail.com>
2022-08-01 07:35:12 +0530

    add music list and geners select feature

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import musicData from "./Components/Data";
 
 function App() {
+  const [selectedMusicList, setSelectedMusicList] = useState([]);
+  const geners = Object.keys(musicData);
+  const onClickSelectGeners = (item) => {
+    const musicList = musicData[item];
+
+    setSelectedMusicList(musicList);
+  };
+  useEffect(() => {
+    // useEffect update state value imediately becoz state is asynchronus
+  }, [selectedMusicList]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav className="nav-bar">
+        <h1>🎧 Music Recommendation 🎧</h1>
+      </nav>
+      <div className="container">
+        <div className="geners">
+          {geners.map((item) => {
+            return (
+              <button
+                className="geners-btn"
+                value={item}
+                onClick={() => onClickSelectGeners(item)}
+                key={item}
+              >
+                {item}
+              </button>
+            );
+          })}
+        </div>
+        <div className="music-list">
+          {selectedMusicList.map((item, idx) => {
+            console.log(item);
+            return <div className="list-item" key={idx}>
+              <img src={item.img} style={style.img} />
+              <p>{item.name}</p>
+            </div>;
+          })}
+        </div>
+      </div>
     </div>
   );
 }
-
+const style = {
+  img:{
+    height:'100px',
+    width:'100px',
+    padding: '0.5rem'
+  }
+}
 export default App;
---
+++ src/Components/Data.js
@@ -0,0 +1,62 @@
+const musicData = {
+  party: [
+    {
+      name: "Kar Gayi Chull",
+      img: "https://a10.gaanacdn.com/gn_img/albums/z8k3y13rxo/8k3yVq1nWr/size_l.webp",
+    },
+    {
+      name: "Dj Waley Babu",
+      img: "https://a10.gaanacdn.com/gn_img/song/R7vKXr6Wmr/vKXm2vNQ3m/size_l_1534500785.webp",
+    },
+    {
+      name: "5 Taara",
+      img: "https://a10.gaanacdn.com/gn_img/albums/dwN398KDPa/wN39Rg8OWD/size_l.webp",
+    },
+    {
+      name: "Naah",
+      img: "https://a10.gaanacdn.com/gn_img/albums/lJvKa56KDV/JvKaMw4nKD/size_l.webp",
+    },
+  ],
+  romance: [
+    {
+      name: "Tera Mera Pyar",
+      img: "https://a10.gaanacdn.com/gn_img/albums/ZaP37RKDy7/aP37LgRWDy/size_l.webp",
+    },
+    {
+      name: "Pehli-pehli Baar Mohabbat Ki Hai",
+      img: "https://a10.gaanacdn.com/gn_img/albums/10q3Z1K52r/10q3ZjQ352/size_l.webp",
+    },
+    {
+      name: "Mera Dil Bhi Kitna Pagal Hai",
+      img: "https://a10.gaanacdn.com/gn_img/albums/a7LWBaz3zX/7LWBexGR3z/size_l.webp",
+    },
+    {
+      name: "Pyar Kiya To Nibhana",
+      img: "https://a10.gaanacdn.com/gn_img/albums/z8k3yd1Krx/z8k3yd1Krx/size_l.webp",
+    },
+    {
+      name: "Tum Hi Ho",
+      img: "https://a10.gaanacdn.com/gn_img/albums/d41WjnWPLq/41Wjz1LOWP/size_l.webp",
+    },
+  ],
+  punjabi: [
+    {
+      name: "Excuses",
+      img: "https://a10.gaanacdn.com/gn_img/albums/10q3Z1K52r/q3ZRZg2035/size_l.jpg",
+    },
+    {
+      name: "Brown Munde",
+      img: "https://a10.gaanacdn.com/gn_img/albums/qa4WEqWP1p/4WEky0e8KP/size_l.webp",
+    },
+    {
+      name: "8 Parche",
+      img: "https://a10.gaanacdn.com/gn_img/albums/ZaP37RKDy7/aP37weZl3D/size_l.webp",
+    },
+    {
+      name: "Badnam",
+      img: "https://a10.gaanacdn.com/gn_img/albums/10q3Z1K52r/0q3ZldvQb5/size_l.webp",
+    },
+  ],
+};
+
+export default musicData;
